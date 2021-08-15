dcd69bfce1a600e79d8f868daa0ee69ae6f0fc69
KayPea <prescottdarling@gmail.com>
2021-08-15 03:30:59 -0400

    basic socketio server set up

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const cors = require("cors");
+const http = require("http");
+const socketIO = require("socket.io");
+
+const port = process.env.PORT || 3030;
+const NEW_MESSAGE_EVENT = "new-message-event";
+
+const app = express();
+const server = http.createServer(app);
+
+const io = socketIO(server, {
+  cors: true,
+  origins: ["localhose:3000"],
+});
+
+app.use(cors());
+
+// Hardcoding a room here - you can create multiple rooms as needed instead
+const room = "general";
+
+io.on("connection", (socket) => {
+  socket.join(room);
+
+  socket.on(NEW_MESSAGE_EVENT, (data) => {
+    io.in(room).emit(NEW_MESSAGE_EVENT, data);
+  });
+
+  socket.on("disconnect", () => {
+    socket.leave(room);
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
